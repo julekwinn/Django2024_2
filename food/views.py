115044d25501e0f449873474cb5f7a451f5036e3
Julek <julek@julek.pl>
2024-04-10 23:03:26 +0200

    Add second view

--- food/views.py
+++ food/views.py
@@ -6,3 +6,7 @@ from django.shortcuts import render
 
 def index(request):
     return HttpResponse("Hello, world. Food page is here!")
+
+
+def item(request):
+    return HttpResponse("<h1>This is an item page</h1>")
