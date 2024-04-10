09405a34af30daf254d228e3f88aeeac1f4360cb
Julek <julek@julek.pl>
2024-04-10 23:22:45 +0200

    Create First Data Model

--- food/models.py
+++ food/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Item(models.Model):
+    item_name = models.CharField(max_length=200)
+    item_desc = models.TextField(max_length=200)
+    item_price = models.IntegerField()
+
