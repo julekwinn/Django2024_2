0b7c0d0c43c57479288e17026487385e964aef4a
Juliusz Winnicki <126665165+julekwinn@users.noreply.github.com>
2024-05-04 23:30:51 +0200

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-"""Django's command-line utility for administsfrative tasdskfdshaffhasadffaddfafggffasfdfcvcxstwffaswggftdfdfvvvgfdsggsfsgfgjdavvvfjhgavvggggfdfsgdfffggfvv
+"""Django's command-line utility for administsfrative tasdskfdshaffhasadffaddfafggffasfdfcvcxstwffffaswggftdfdfvvvgfdsggsfsgfgjdavvvfjhgavvggggfdfsgdfffggfvv
 vvcvvttbvfnmbnmnfdffh."""
 import os
 import sys
