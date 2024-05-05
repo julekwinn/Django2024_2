371d5904bddd5f339a85186b9376ef31db36fd87
Your Name <you@example.com>
2024-05-05 19:05:37 +0200

    Add dynamic url

--- food/views.py
+++ food/views.py
@@ -22,4 +22,8 @@ def item(request):
 
 
 def detail(request, item_id):
-    return HttpResponse("Tjis is item no/id: %s" % item_id)
+    item = Item.objects.get(pk=item_id)
+    context = {
+        'item':item
+    }
+    return render(request,'food/detail.html',context)
