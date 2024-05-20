9c674760a55564211ed101b105d865fb573c7dd6
julekwinn <julek.winn@gmail.com>
2024-05-21 00:54:40 +0200

    Add saving user funcionality

--- users/views.py
+++ users/views.py
@@ -9,6 +9,8 @@ def register(request):
     if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
+
+            form.save() # Zapisanie uzytkownika do bazy danych
             username = form.cleaned_data.get('username')
             messages.success(request, f"Welcome {username}, your account is created!")
             return redirect('food:index')
