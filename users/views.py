518e2e5aefbdc2e9d786b3c12c5a2e32b40b235f
julekwinn <julek.winn@gmail.com>
2024-05-22 01:02:08 +0200

    Add email field to register account

--- users/views.py
+++ users/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from .forms import RegisterForm
 # Create your views here.
 
 
 def register(request):
 
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = RegisterForm(request.POST)
         if form.is_valid():
 
             form.save() # Zapisanie uzytkownika do bazy danych
@@ -16,5 +17,5 @@ def register(request):
             return redirect('food:index')
 
     else:
-        form = UserCreationForm()
+        form = RegisterForm()
     return render (request,"users/register.html", {"form": form})
