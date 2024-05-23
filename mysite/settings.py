32ce7d88c1d28d1e23922e79d4c87fc9615fff60
julekwinn <julek.winn@gmail.com>
2024-05-24 00:29:15 +0200

    Adding Path to Upload Image

--- mysite/settings.py
+++ mysite/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -131,3 +131,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 LOGIN_REDIRECT_URL = 'food:index'
 
 LOGIN_URL = 'login' # Jesli jakis widok wymasga bycia zalogowanym przekierowje na ten adres url
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'pictures')
+
+MEDIA_URL = '/pictures/'
